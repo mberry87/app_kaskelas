f6c00b46d51312a1abd1396b033e3b8f453e177b
mberry87 <berry311287@gmail.com>
2022-08-08 20:34:58 +0700

    first commit

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer">
+        <strong>Copyright &copy; By StrawBerry | Tanjungpinang.</strong>
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 1.0.0
+        </div>
+    </div>
+</footer>
